69cbc926690920fdab1461298ed1f670d273e4ee
sapuska <sagajenniina@gmail.com>
2024-03-13 17:48:28 +0200

    Matikkapeli-tehtävä tehty.

---
+++ js/functions.js
@@ -0,0 +1,39 @@
+'use strict'
+
+let rand_num1 = 0
+let rand_num2 = 0
+let countCorrect = 0
+let countIncorrect = 0
+
+const getRandomIntNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomIntNumberInRange(1,10)
+    rand_num2 = getRandomIntNumberInRange(1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+});
+
+document.querySelector('button').addEventListener('click',() => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        alert('Correct!')
+        countCorrect++
+    } else {
+        alert('Incorrect!')
+        countIncorrect++
+    }
+
+    document.querySelector('#countCorrect').innerHTML = countCorrect
+    document.querySelector('#countIncorrect').innerHTML = countIncorrect
+
+    randomizeNumbers()
+    document.querySelector('input').value=''
+})
